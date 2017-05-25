a833721ab38f5bc4546dc85cdf0c07bcde0323b8
Pavel Vorobyov <aquavitale@yandex.ru>
2017-05-25 23:21:40 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Xcute",
+    version="0.7.1",
+    packages=find_packages(),
+    scripts=["x"],
+    install_requires=["gevent", "requests", "termcolor", "progressbar", "gnureadline"],
+    author="Pavel Vorobyov",
+    author_email="aquavitale@yandex.ru",
+    description="Parallel execution command-line tool",
+    license="MIT",
+    keywords="executer xcute conductor",
+    url="https://github.com/viert/xcute"
+)
