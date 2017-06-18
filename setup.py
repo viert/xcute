f367a4c2852e72471ac3f03b67c41552d321b742
Pavel Vorobyov <aquavitale@yandex.ru>
2017-06-18 16:15:22 +0300

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Xcute",
-    version="0.7.6",
+    version="0.7.7",
     packages=find_packages(),
     scripts=["x"],
     install_requires=["gevent", "requests", "termcolor", "progressbar", "gnureadline"],
