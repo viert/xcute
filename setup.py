bfc37367e57eb4b81389bd164d30e03ce5488ec7
Воробьев Павел <p.vorobyov@corp.mail.ru>
2017-11-14 15:29:53 +0300

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Xcute",
-    version="0.9.0",
+    version="0.9.1",
     packages=find_packages(),
     scripts=["x"],
     install_requires=["gevent", "requests", "termcolor", "progressbar", "gnureadline", "pyparsing"],
